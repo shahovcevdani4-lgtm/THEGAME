d960aef461493b4d259bc85a6441f64c6dda2687
shahovcevdani4-lgtm <shahovcevdani4@gmail.com>
2025-11-08 13:41:12 +0300

    Add world characters and update agility terminology

---
+++ data/characters.py
@@ -0,0 +1,15 @@
+"""Definitions of non-hostile characters encountered in the world."""
+
+CHARACTERS = {
+    "warlock": {
+        "name": "WARLOCK",
+        "char": "§",
+        "fg": (180, 0, 200),
+        "bg": (20, 0, 40),
+        "stats": {
+            "str": 4,
+            "agi": 6,
+            "int": 8,
+        },
+    },
+}
